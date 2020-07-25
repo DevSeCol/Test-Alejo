d33b35b2a8bb096c36605fff8b83025d68dd889d
Losano <Losano@gmail.com>
2020-07-25 17:01:49 -0500

    adicion carpeta terraform

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+resource "google_container_cluster" "primary-cluster" {
+  name                     = "devsecol-cluster"
+  location                 = var.region
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  #network                  = var.cluster_network
+  #subnetwork               = var.cluster_subnetwork
+}
+
+resource "google_container_node_pool" "primary-pool" {
+  name       = "primary-pool"
+  location   = google_container_cluster.primary-cluster.location
+  cluster    = google_container_cluster.primary-cluster.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "g1-small"
+
+    #service_account = var.service_account
+
+    labels = {
+      machine-type = "preemtible"
+    }
+
+    tags = ["spark-cluster"]
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
